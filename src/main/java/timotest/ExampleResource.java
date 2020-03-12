0a0c0f1d258516074d254908dcdb973cdbc5cd9d
Timo Friman <tfriman@redhat.com>
2020-03-12 10:54:28 +0200

    Added logging.

--- src/main/java/timotest/ExampleResource.java
+++ src/main/java/timotest/ExampleResource.java
@@ -1,8 +1,8 @@
 package timotest;
 
-import org.eclipse.microprofile.metrics.MetricUnits;
-import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -12,14 +12,17 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class ExampleResource {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ExampleResource.class);
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Timed(name = "helloEndpoint")
     public DomainObject hello() {
+        LOG.info("/hello called");
         return new DomainObject("hello");
     }
 
-    private class DomainObject {
+    private static class DomainObject {
         private final String message;
 
         public DomainObject(String hello) {

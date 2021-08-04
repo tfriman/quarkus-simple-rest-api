6e2cc5cf2820f56a4c252a40b0c102f1bb19f796
Timo Friman <tjf@iki.fi>
2021-08-04 19:30:12 +0300

    Updated to Quarkus 2.1.1.Final and changed smallrye metrics to micrometer one.

--- src/main/java/timotest/ExampleResource.java
+++ src/main/java/timotest/ExampleResource.java
@@ -8,34 +8,55 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-import java.nio.file.*;
-import java.util.Iterator;
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+import java.time.LocalDateTime;
+import java.util.Random;
 
 @Path("/hello")
 public class ExampleResource {
 
     private static final Logger LOG = LoggerFactory.getLogger(ExampleResource.class);
 
+    private final Random random = new Random();
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Timed(name = "helloEndpoint")
-    public DomainObject hello() {
+    public DomainObject hello() throws InterruptedException {
         LOG.info("/hello called");
+        // make some variation to response time
+        Thread.sleep(random.nextInt(200));
         return new DomainObject("hello");
     }
 
     private static class DomainObject {
         private final String message;
+        private final LocalDateTime localDateTime;
+        private final String hostname;
 
         public DomainObject(String hello) {
             this.message = hello;
+            this.localDateTime = LocalDateTime.now();
+            String hostnameTmp = "unknown";
+            try {
+                hostnameTmp = Inet4Address.getLocalHost().getHostName();
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+            this.hostname = hostnameTmp;
         }
 
         public String getMessage() {
             return message;
+        }
+
+        public LocalDateTime getLocalDateTime() {
+            return localDateTime;
+        }
+
+        public String getHostname() {
+            return hostname;
         }
     }
 }
